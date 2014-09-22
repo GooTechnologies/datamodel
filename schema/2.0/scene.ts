0c53f16c4f44af6bf242c4993ae88f4f836b13f8
Simon Howe <simon.howe@gooengine.com>
2014-09-22 12:30:21 +0200

    merge project attributes into scenes

--- schema/2.0/scene.ts
+++ schema/2.0/scene.ts
@@ -7,10 +7,49 @@ enum CanvasSizingMode {
 	AspectRatio
 };
 
+
+// REVIEW: use an enum, see above
+
+/**
+ * Matches types with one of 2d or 3d
+ * (case sensitive, must be lowercase)
+ *
+ * @type string
+ * @pattern (2d|3d)
+ */
+interface SceneType {}
+
 /**
  * A scene in create (what was previously called a project)
  */
 interface scene extends GooObject {
+
+	type?: SceneType;
+
+	tags?: {
+	    [tagName: string]: string;
+	}
+
+	/**
+	* All references not in scene.entities
+	*/
+	assets?: {
+		[listId: string]: {
+			sortValue: number;
+			assetRef: string;
+		}
+	}
+
+	// Contains the stable url for the latest published project.
+	publishedUrl?: URI;
+
+	/**
+	*	A set with published URI with it's date of publish.
+	*/
+	publishedUrls?: {
+		[URI: string]: DateTime
+
+	}
 
 	/**
 	 * _Root_ entities that are present in the scene
